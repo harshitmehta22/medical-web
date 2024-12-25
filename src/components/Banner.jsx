7a2f149f22d1ccccacb6ee362dae2ac4c5e26aba
Harshit <harshitmehta155@gmail.com>
2024-12-25 17:29:15 +0530

    File Changes

---
+++ src/components/Banner.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Banner = () => {
+    return(
+        <>
+        	<section class="slider">
+			<div class="hero-slider">
+				<div class="single-slider" style={{background: 'url(./images/slider2.jpg)'}}>
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-7">
+								<div class="text">
+									<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+									<div class="button">
+										<a href="#" class="btn">Get Appointment</a>
+										<a href="#" class="btn primary">Learn More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+        </>
+    )
+}
+export default Banner;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import react from 'react'
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+
+
+const Home = () => {
+    return(
+        <>
+        <div>
+            <Header/>
+            <Banner/>
+        </div>
+        </>
+    )
+}
+export default Home;
